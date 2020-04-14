c5eec19dea7b992e0dc9977d56b877b47c5415ff
Forest Handford <foresthandford@gmail.com>
2020-04-14 16:59:05 -0400

    Issue #12: Added 3 tests for the Company model

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -3,7 +3,22 @@
 require 'test_helper'
 
 class CompanyTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save company without name' do
+    company = Company.new
+    assert_not company.save, 'Saved company without name!'
+  end
+
+  test 'should not save company when CEO < 4 chars' do
+    company = Company.new
+    company.name = 'Test Co.'
+    company.ceo = 'Ed'
+    assert_not company.save, 'Saved company with 2 charachter CEO!'
+  end
+
+  test 'should save valid company' do
+    company = Company.new
+    company.name = 'Valid Co.'
+    company.ceo = 'Ed Valid'
+    assert company.save, 'Failed to save company with valid info!'
+  end
 end
