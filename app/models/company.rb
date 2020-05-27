f216ac0575a7be55ae1d47b0e2fd77364d16995d
Forest Handford <forest.handford@gmail.com>
2020-05-27 09:08:41 -0400

    Added Exec Diversity to Company Index

--- app/models/company.rb
+++ app/models/company.rb
@@ -10,8 +10,8 @@ class Company < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
   validates :ceo, length: { minimum: 4 }
 
-  def exec_diverse_score_html
-    str = exec_gender_score + exec_race_score
+  def exec_diverse_score_html(img_height = 120)
+    str = exec_gender_score(img_height) + exec_race_score(img_height)
     # rubocop:disable Rails/OutputSafety
     str.html_safe
     # rubocop:enable Rails/OutputSafety
@@ -19,31 +19,43 @@ class Company < ApplicationRecord
 
   private
 
-  def exec_gender_score
+  def non_male_count
     non_male = ceo_gender == 'M' ? 0 : 1
 
     executives.each do |exec|
       non_male += 1 unless exec.gender == 'M'
     end
 
-    if (non_male + 0.0) / (executives.count + 1) > 0.24999
-      "<img src='/gender_diverse_exec.png' alt='Gender Inclusive Executive Team'>"
+    non_male
+  end
+
+  def exec_gender_score(img_height)
+    if (non_male_count + 0.0) / (executives.count + 1) > 0.24999
+      text = 'Gender Inclusive Executive Team'
+      "<img src='/gender_diverse_exec.png' height='#{img_height}' alt='#{text}' title='#{text}'>"
     else
-      "<img src='/gender_undiverse_exec.png' alt='Gender Exclusionary Executive Team'>"
+      text = 'Gender Exclusionary Executive Team'
+      "<img src='/gender_undiverse_exec.png' height='#{img_height}' alt='#{text}' title='#{text}'>"
     end
   end
 
-  def exec_race_score
+  def non_white_count
     non_white = ceo_race == 'W' ? 0 : 1
 
     executives.each do |exec|
       non_white += 1 unless exec.race == 'W'
     end
 
-    if (non_white + 0.0) / (executives.count + 1) > 0.24999
-      "<img src='/race_diverse_exec.png' alt='Racially Inclusive Executive Team'>"
+    non_white
+  end
+
+  def exec_race_score(img_height)
+    if (non_white_count + 0.0) / (executives.count + 1) > 0.24999
+      text = 'Racially Inclusive Executive Team'
+      "<img src='/race_diverse_exec.png' height='#{img_height}' alt='#{text}' title='#{text}'>"
     else
-      "<img src='/race_undiverse_exec.png' alt='Racially Exclusionary Executive Team'>"
+      text = 'Racially Exclusionary Executive Team'
+      "<img src='/race_undiverse_exec.png' height='#{img_height}' alt='#{text}' title='#{text}'>"
     end
   end
 end
