1b53ab17e0eae5b6db1bc1e9fb62c029952cd56c
Forest Handford <forest.handford@gmail.com>
2020-05-27 17:39:53 -0400

    Issue #16: Migrated to Postgres

--- db/migrate/20200419192716_add_user_info_to_users.rb
+++ db/migrate/20200419192716_add_user_info_to_users.rb
@@ -3,15 +3,17 @@
 # Adding More User Fields
 class AddUserInfoToUsers < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :name, :string
-    add_column :users, :private, :boolean
-    add_column :users, :year_of_birth, :string
-    add_column :users, :gender, :string
-    add_column :users, :race, :string
-    add_column :users, :lgbtqia_plus, :boolean
-    add_column :users, :immigrant, :boolean
-    add_column :users, :expat, :boolean
-    add_column :users, :has_disability, :boolean
-    add_column :users, :veteran, :boolean
+    change_table :users, bulk: true do |t|
+      t.string :name
+      t.boolean :private
+      t.string :year_of_birth
+      t.string :gender
+      t.string :race
+      t.boolean :lgbtqia_plus
+      t.boolean :immigrant
+      t.boolean :expat
+      t.boolean :has_disability
+      t.boolean :veteran
+    end
   end
 end
