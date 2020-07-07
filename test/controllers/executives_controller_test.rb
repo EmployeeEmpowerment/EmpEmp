f874e83ee329c2bbb58b1be1efe5d0d4596b1987
Forest Handford <foresthandford@gmail.com>
2020-07-06 23:07:30 -0400

    Issue #57: Fixed CEO Compensation Issues
    * Also added tests so this kind of issue never recurs

--- test/controllers/executives_controller_test.rb
+++ test/controllers/executives_controller_test.rb
@@ -3,7 +3,27 @@
 require 'test_helper'
 
 class ExecutivesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test 'should get edit' do
+    sign_in users(:one)
+    get edit_company_executive_url(companies(:facebook), executives(:one))
+    assert_response :success
+  end
+
+  test 'should get redirected for edit w/o user' do
+    get edit_company_executive_url(companies(:facebook), executives(:one))
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'should get redirected for new w/o user' do
+    get new_company_executive_url(companies(:facebook))
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'should get new' do
+    sign_in users(:one)
+    get new_company_executive_url(companies(:facebook))
+    assert_response :success
+  end
 end
--- test/controllers/jobs_controller_test.rb
+++ test/controllers/jobs_controller_test.rb
@@ -3,7 +3,38 @@
 require 'test_helper'
 
 class JobsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  include Devise::Test::IntegrationHelpers
+
+  test 'should get redirected for index w/o user' do
+    get jobs_url
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'should get index' do
+    sign_in users(:one)
+    get jobs_url
+    assert_response :success
+  end
+
+  test 'should get redirected for edit w/o user' do
+    get edit_job_url(jobs(:facebook_cashier))
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'should get redirected for edit w/ wrong user' do
+    sign_in users(:two)
+    get edit_job_url(jobs(:facebook_cashier))
+    assert_redirected_to root_path
+  end
+
+  test 'should get redirected for new w/o user' do
+    get new_job_url
+    assert_redirected_to new_user_session_url
+  end
+
+  test 'should get new' do
+    sign_in users(:one)
+    get new_job_url
+    assert_response :success
+  end
 end
